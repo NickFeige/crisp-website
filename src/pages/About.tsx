b1a6d135e3e8b2c43226ad8491b2ab963c6e708f
NickFeige <nfeige7@gmail.com>
2025-07-02 11:38:07 +0100

    new routing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
 const About: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate('/home');
-  };
   return (
     <div className="main-background">
       <div className="about-content">
-        <button className="title" onClick={() => navigate('/Home')}><b>CRISP</b></button>
+        {/* CRISP title as a button that routes to /home */}
+        <button className="title" onClick={() => navigate('/Home')}>
+          <b>CRISP</b>
+        </button>
+
         <h2 className="title2">About Crisp</h2>
-        <p className="about-text">Crisp your local fresh fruit and veg shop has been providing
-            produce to the community for 18 years </p>
+        <p className="about-text">
+          Crisp, your local fresh fruit and veg shop, has been providing produce to the community for 18 years.
+        </p>
       </div>
     </div>
   );
